bc8e9b50cb1cab06f2a5d2ca368dcee9c8c01492
JrzenonDev <jrobertoonb@gmail.com>
2022-08-12 01:04:41 -0300

    create fake backend for tests auth tokens

---
+++ services/auth.ts
@@ -0,0 +1,19 @@
+import { v4 as uuid } from 'uuid';
+
+type SignInRequestData = {
+  email: string;
+  password: string;
+}
+
+const delay = (amount = 750) => new Promise(resolve => setTimeout(resolve, amount))
+
+export async function signInRequest(data) {
+  await delay()
+
+  return {
+    token: uuid(),
+    user: {
+      
+    }
+  }
+}
